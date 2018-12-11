96fdfb5956c7c944065a405b2d12e674cd6eb647
tonedblue <adeintokyo@gmail.com>
2018-12-11 10:43:10 +0900

    Presentation implementation

---
+++ CleanArchitecture.Presentation/Controllers/AuthorsController.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchitecture.Application.Models.Authors.Commands.UpdateAuthor;
+using CleanArchitecture.Application.Models.Authors.CreateAuthor.Commands;
+using CleanArchitecture.Application.Models.Authors.GetAllAuthors.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArchitecture.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthorsController : BaseController
+    {
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetAllAuthorsModel>>> Get()
+        {
+            return Ok(await Mediator.Send(new GetAllAuthorsQuery()));
+        }
+
+        // POST: api/<controller>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]CreateAuthorCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        // POST: api/<controller>
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update([FromRoute] int id, [FromBody] UpdateAuthorCommand command)
+        {
+            if (id != command.AuthorId)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
---
+++ CleanArchitecture.Presentation/Startup.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchitecture.Application.Interfaces.Authors;
+using CleanArchitecture.Application.Interfaces.Books;
+using CleanArchitecture.Application.Models.Authors.GetAllAuthors.Queries;
+using CleanArchitecture.Persistence.Repositories.Authors;
+using CleanArchitecture.Persistence.Repositories.Books;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Presentation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            
+            // Author Mediatr
+            services.AddMediatR(typeof(GetAllAuthorsQuery).GetTypeInfo().Assembly);
+            services.AddMediatR(typeof(GetAllAuthorsQuery).GetTypeInfo().Assembly);
+            services.AddMediatR(typeof(GetAllAuthorsQuery).GetTypeInfo().Assembly);
+
+            services.AddTransient<IAuthorRepository, AuthorRepository>();
+            services.AddTransient<IBookRepository, BookRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
